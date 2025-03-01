5de8187df14acbd5fd6dda3bf7aaa71603c02784
Deny Febriyanto <denyfebri@gmail.com>
2025-03-02 00:31:10 +0700

    Belajar Maret JS

---
+++ Dasar/04 Pop Up/script.js
@@ -0,0 +1,11 @@
+alert("Selamat datang!");
+var lagi = true;
+
+while (lagi === true) {
+  var nama = prompt("Masukkan nama:");
+  alert("Ooo kamu " + nama);
+
+  var lagi = confirm("Kamu mau nama yang lain " + nama + " ?");
+}
+
+alert("Get the fuuck out " + nama);
