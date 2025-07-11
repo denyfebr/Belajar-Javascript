6e91d907c83c5fe22c9278b6e3552e23f849c7e9
denyfebr <denyfebri@gamil.com>
2025-07-11 07:42:55 +0700

    Pengenalan Object

---
+++ WPU/Dasar/26 ObjectPengenalan/script.js
@@ -0,0 +1,11 @@
+// membuat Object
+var mhs = {
+    nama : "Deny Febriyanto",
+    umur : 37,
+    ips : [3.00, 2.50, 3.20],
+    alamat : {
+      jalan : "Jl. Fifth Avenue 5",
+      kota : "New York",
+      province : "Manhattan"
+    }  
+};
