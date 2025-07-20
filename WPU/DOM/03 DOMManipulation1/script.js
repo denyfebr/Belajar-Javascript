71f80d201e0330ab3ffd641852971e831c650195
Deny Febriyanto <denyfebri@gmail.com>
2025-07-20 21:58:33 +0700

    DOM manipulasi

---
+++ WPU/DOM/03 DOMManipulation1/script.js
@@ -0,0 +1,36 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em><b>Deny</b></em> Febriyanto';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = '<div><p>paragraf 1</p></div>';
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name', 'deny');
+// const a = document.querySelector('section#a a');
+// a.getAttribute('href');
+// untuk meremove attribut
+// a.removeAttribute('href');
+
+const p2 = document.querySelector('.p2');
+// p2.setAttribute('class', 'label');
+
+// class
+// element.classList.add();
+// element.classList.remove();
+
+// element.classList.toggle();
+// p2.classList.toggle('biru-muda'); -> jika tidak ada class 'biru muda' maka ditambahkan dan kembalian nilai true,
+// jika ada class 'biru muda' maka dihapus dan kembalian nilai false
+
+// element.classList.item();
+// p2.classList.item(2); -> keluar nama class di index sesuai argumen. contoh 'label'/'pop up'/'modal'
+
+// element.classList.contains();
+// p2.classList.contains('modal'); -> keluar true jika ada, false jika tidak ada
+
+// element.classList.replace();
+// p2.classList.replace('tiga','empat')
