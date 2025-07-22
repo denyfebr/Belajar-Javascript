99fac52ae8eafac9d711408ed433b952ad12bdd3
Deny Febriyanto <denyfebri@gmail.com>
2025-07-23 03:41:07 +0700

    DOM latihan warna

--- WPU/DOM/05 DOMWarna/script.js
+++ WPU/DOM/05 DOMWarna/script.js
@@ -1,21 +1,21 @@
 const tUbahWarna = document.getElementById('tUbahWarna');
-tUbahWarna.onclick = function(){
-    // document.body.style.backgroundColor = 'lightblue';
-    // document.body.setAttribute('class','biru-muda');
-    document.body.classList.toggle('biru-muda');
-}
+tUbahWarna.onclick = function () {
+  // document.body.style.backgroundColor = 'lightblue';
+  // document.body.setAttribute('class','biru-muda');
+  document.body.classList.toggle('biru-muda');
+};
 
 const tAcakWarna = document.createElement('button');
 const teksTombol = document.createTextNode('Acak');
 tAcakWarna.appendChild(teksTombol);
-tAcakWarna.setAttribute('type','button');
+tAcakWarna.setAttribute('type', 'button');
 tUbahWarna.after(tAcakWarna);
 
-tAcakWarna.addEventListener('click',function(){
-    const r = Math.round(Math.random()*255+1);
-    const g = Math.round(Math.random()*255+1);
-    const b = Math.round(Math.random()*255+1);
-    document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+tAcakWarna.addEventListener('click', function () {
+  const r = Math.round(Math.random() * 255 + 1);
+  const g = Math.round(Math.random() * 255 + 1);
+  const b = Math.round(Math.random() * 255 + 1);
+  document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 });
 
 const sMerah = document.querySelector('input[name=sMerah]');
@@ -28,16 +28,16 @@ lNilaiWarnaMerah.setAttribute('class', 'teksmerah');
 // Sisipkan label nilai di bawah slider merah
 sMerah.after(lNilaiWarnaMerah);
 
-sMerah.addEventListener('input', function() {
-    const r = sMerah.value;
-    const g = sHijau.value;
-    const b = sBiru.value;
+sMerah.addEventListener('input', function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
 
-    // Ubah background
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+  // Ubah background
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    // Perbarui isi teks
-    lNilaiWarnaMerah.textContent = `Merah: ${r}`;
+  // Perbarui isi teks
+  lNilaiWarnaMerah.textContent = `Merah: ${r}`;
 });
 
 const lNilaiWarnaHijau = document.createElement('p');
@@ -46,15 +46,15 @@ lNilaiWarnaHijau.setAttribute('class', 'tekshijau');
 // Sisipkan label nilai di bawah slider hijau
 sHijau.after(lNilaiWarnaHijau);
 
-sHijau.addEventListener('input',function(){
-    const r = sMerah.value;
-    const g = sHijau.value;
-    const b = sBiru.value;
-    // Ubah background
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+sHijau.addEventListener('input', function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  // Ubah background
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    // Perbarui isi teks
-    lNilaiWarnaHijau.textContent = `Hijau: ${g}`;
+  // Perbarui isi teks
+  lNilaiWarnaHijau.textContent = `Hijau: ${g}`;
 });
 
 const lNilaiWarnaBiru = document.createElement('p');
@@ -63,13 +63,28 @@ lNilaiWarnaBiru.setAttribute('class', 'teksbiru');
 // Sisipkan label nilai di bawah slider biru
 sBiru.after(lNilaiWarnaBiru);
 
-sBiru.addEventListener('input',function(){
-    const r = sMerah.value;
-    const g = sHijau.value;
-    const b = sBiru.value;
-     // Ubah background
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+sBiru.addEventListener('input', function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  // Ubah background
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    // Perbarui isi teks
-    lNilaiWarnaBiru.textContent = `Biru: ${b}`;
+  // Perbarui isi teks
+  lNilaiWarnaBiru.textContent = `Biru: ${b}`;
+});
+
+document.body.addEventListener('mousemove', function (event) {
+  // posisi mouse
+  // console.log(event.clientX);
+  // ukuran browser
+  // console.log(window.innerWidth);
+
+  const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+  console.log(xPos);
+
+  const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+  console.log(yPos);
+
+  document.body.style.backgroundColor = `rgb(${xPos},${yPos},100)`;
 });
