0e24b5ccbbfea479fe5bdc473c56bd9e0127cde6
Deny Febriyanto <denyfebri@gmail.com>
2025-08-02 04:31:20 +0700

    Arrow Function This

---
+++ WPU/Lanjutan/07 ArrowFunctionThis/script.js
@@ -0,0 +1,72 @@
+// Konsep this pada Arrow Function
+
+// Constructor Function
+// const Suppliers = function() {
+//     this.nama = 'Gandaerah Hedana';
+//     this.volume = 350;
+//     this.sayHello = function() {
+//         console.log(`Saya supplier ${this.nama}, dan volume pasokan ${this.volume} Ton`);
+//     }
+// }
+
+// const gandaerah = new Suppliers();
+
+// Arrow function
+
+// const Suppliers = function() { // constructor tidak bisa dijadikan arrow function
+//     this.nama = 'Gandaerah Hedana';
+//     this.volume = 350;
+//     this.sayHello = () => { // bisanya function expression
+//         console.log(`Saya supplier ${this.nama}, dan volume pasokan ${this.volume} Ton`);
+//     }
+// }
+
+// const gandaerah = new Suppliers();
+
+// Object literal
+// const mhs1 = {
+//     nama: 'Gandaerah',
+//     volume: 320,
+//     sayHello: () => {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.volume}`);
+// akan error
+//     }
+// }
+
+// const Suppliers = function () {
+//   this.nama = 'Gandaerah Hedana';
+//   this.volume = 350;
+
+//   // function expression ga kena hoisting
+//   this.sayHello = function () {
+//     console.log(`Saya supplier ${this.nama}, dan volume pasokan ${this.volume} Ton`);
+//   };
+
+//   // function declaration kena hoisting
+//   setInterval(() => {
+//     console.log(this.volume++);
+//   }, 500);
+// };
+
+// const gandaerah = new Suppliers();
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function () {
+  let satu = 'size';
+  let dua = 'caption';
+
+  if (this.classList.contains(satu)) {
+    // satu = temp;
+    // satu = dua;
+    // dua = temp;
+    [satu, dua] = [dua, satu];
+  }
+
+  //let that = this; //diakalin
+  // console.log(this);
+  this.classList.toggle(satu);
+  setTimeout(() => {
+    // this.classList.toggle('caption'); isinya this jadi windows karena hoisting
+    this.classList.toggle(dua);
+  }, 600);
+});
