7031cbca010e29dd42fd1c2cad60ffded271bab8
denyfebr <denyfebri@gamil.com>
2025-08-22 10:10:29 +0700

    Tagged Template Literal

---
+++ WPU/Lanjutan/12 Tagged Template Literal/script.js
@@ -0,0 +1,30 @@
+// Tagged Templates
+// const nama = 'Deny Febriyanto';
+// const umur = 33;
+
+// function coba(strings, ...values){
+//     // let result = '';
+//     // strings.forEach((str, i) => {
+//     //     result += `${str}${values[i] || ''}`;
+//     // });
+//     // return result;
+
+//     return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`,'');
+// }
+
+// const str = coba `Halo, nama saya ${nama}, saya ${umur} tahun.`;
+// console.log(str);
+
+// Highlight
+
+const nama = 'Deny Febriyanto';
+const umur = 33;
+const email = 'denyfebri@gmail.com';
+
+function highlight(strings, ...values){   
+    return strings.reduce((result, str, i) => `${result}${str}<span class = "hl">${values[i] || ''}</span>`,'');
+}
+
+const str = highlight`Halo, nama saya ${nama}, saya ${umur} tahun, dan email saya adalah : ${email}`;
+
+document.body.innerHTML = str;
