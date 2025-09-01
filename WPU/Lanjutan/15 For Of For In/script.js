dddcd1cfc2dc6c98194ac6c2edec29e490a569af
Deny Febriyanto <denyfebri@gmail.com>
2025-09-01 10:41:21 +0700

    For of

---
+++ WPU/Lanjutan/15 For Of For In/script.js
@@ -0,0 +1,16 @@
+// for..of
+// Array
+const mhs = ['Sandhika', 'Doddy', 'Erik'];
+
+// looping biasa
+// for (let i = 0; i < mhs.length; i++) {
+//   console.log(mhs[i]);
+// }
+
+// looping forEach
+// mhs.forEach((m) => console.log(m));
+
+// looping forOf
+// for (const m of mhs) {
+//   console.log(m);
+// }
