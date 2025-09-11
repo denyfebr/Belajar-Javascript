41fe18a0ae3e61c79eb1ae830194e2a8deef21c4
denyfebr <denyfebri@gamil.com>
2025-09-11 16:26:26 +0700

    Callback Latihan

--- WPU/Lanjutan/21 Callback Latihan/script.js
+++ WPU/Lanjutan/21 Callback Latihan/script.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: 'http://www.omdbapi.com/?i=tt3896198&apikey=e5d25b11&s=avenger',
+    url: 'http://www.omdbapi.com/?apikey=e5d25b11&s=avenger',
     success: results => {
         const movies = results.Search;
         let cards = '';
@@ -10,13 +10,42 @@ $.ajax({
                                 <div class="card-body">
                                     <h5 class="card-title">${m.Title}</h5>
                                     <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                                    <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.}">Show details</a>
+                                    <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show details</a>
                                 </div>
                             </div>                
                        </div>`;
         });
         $('.movie-container').html(cards);
-        // console.log(movies);
+        //console.log(movies);
+
+        // ketika tombol detail di-klik
+        // jangan pakai arrow function krn arrow function tdk mengenal konsep this
+        $('.modal-detail-button').on('click', function(){
+            //console.log($(this).data('imdbid'));
+            $.ajax({
+                url: 'http://www.omdbapi.com/?apikey=e5d25b11&i='+$(this).data('imdbid'),
+                success: m => {
+                    const movieDetail = `<div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                <img src="${m.Poster}" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-md">
+                                                <ul class="list-group">
+                                                    <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                                                    <li class="list-group-item"><strong>Director : </strong> ${m.Director}</li>
+                                                    <li class="list-group-item"><strong>Actors : </strong> ${m.Actors}</li>
+                                                    <li class="list-group-item"><strong>Writer : </strong> ${m.Writer}</li>
+                                                    <li class="list-group-item"><strong>Plot : </strong> <br> ${m.Plot}</li>
+                                                </ul>
+                                                </div>
+                                            </div>
+                                        </div>`;
+                    $('.modal-body').html(movieDetail);
+                }
+            })
+        });
+        
     },
     error: (e) => {
         console.log(e.responseText);
