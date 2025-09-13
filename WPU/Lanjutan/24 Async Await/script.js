c5b17cd6a63390523b1d79234f8fd39a4a8e1fdd
Deny Febriyanto <denyfebri@gmail.com>
2025-09-13 19:59:08 +0700

    Async Await

---
+++ WPU/Lanjutan/24 Async Await/script.js
@@ -0,0 +1,48 @@
+// const coba = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve('selesai');
+//   }, 2000);
+// });
+
+// coba.then(() => console.log(coba));
+//console.log(coba);
+
+function cobaPromise() {
+  return new Promise((resolve, reject) => {
+    let start = performance.now(); // waktu mulai
+
+    for (let i = 0; i < 1000000; i++) {
+      // looping simulasi
+      let x = i * 2;
+    }
+
+    let end = performance.now(); // waktu selesai
+
+    const waktu = end - start;
+
+    if (waktu < 5) {
+      setTimeout(() => {
+        resolve(`selesai pada waktu ${Math.ceil(waktu)} ms`);
+      }, waktu);
+    } else {
+      setTimeout(() => {
+        reject(`kelamaan ancok! ${Math.ceil(waktu)} ms`);
+      }, waktu);
+    }
+  });
+}
+
+// Belum Async dan Await
+// const coba = cobaPromise();
+// coba.then((coba) => console.log(coba)).catch((coba) => console.log(coba));
+
+async function cobaAsync() {
+  try {
+    const coba = await cobaPromise();
+    console.log(coba);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+cobaAsync();
